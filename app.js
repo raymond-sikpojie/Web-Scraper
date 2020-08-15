54722a9d4f79fd893cf642532d0cf636e98fbab5
raymond <raymondomon@yahoo.com>
2020-08-15 19:07:31 +0100

    scrapes a web page and creates a csv file to store the data obtained

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
+const fs = require("fs");
+
+const writeStream = fs.createWriteStream("post.csv");
 
 const request = async () => {
   // Make a fetch request to get the webpage
@@ -15,13 +18,23 @@ const request = async () => {
     const $ = cheerio.load(htmlPage);
 
     // Get a subHeading which has a classname of "services"
-    const subHeading = $(".services");
+    // const subHeading = $(".services");
 
     // console.log(subHeading.html())
     // console.log(subHeading.text());
     // const output = subHeading.children("ul").html();
-    const output = subHeading.find("ul").html();
-    console.log(output);
+    // const output = subHeading.find("li").text();
+
+    // Get all the text in list items found inside an element with classname of "services"
+    $(".services li").each((i, el) => {
+      // write a regex to remove the space in between
+      const listItem = $(el).text().replace(/\s\s+/g, "");
+
+      // write all list items to csv file "\n inserts a new line"
+      writeStream.write(`${listItem} \n`);
+    });
+    console.log("web scraping done.");
+    // console.log(output);
   } catch (error) {
     console.log(error);
   }
